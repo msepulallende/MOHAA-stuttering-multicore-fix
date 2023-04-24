83183211381aa09dadce45c0e243dbf9171248ad
Mario Sepulveda <mario.sepulveda1901@alumnos.ubiobio.cl>
2023-04-24 18:12:34 -0400

    First Version

---
+++ MOHAAFixStutteringMultiCore.bat
@@ -0,0 +1,9 @@
+color 06
+echo START
+echo MOHAA STUTTERING FIX
+echo EN: PRESS ANY KEY TO START THE GAME
+pause
+cd C:\"Program Files (x86)"\"GOG Galaxy"\Games\"Medal of Honor - Allied Assault War Chest"
+start /affinity 1 MOHAA.exe
+echo NOW, ENJOY THE GAME WITHOUT STUTTERING :D
+pause
