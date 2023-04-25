26e73f951a13367f63dda96bc4d629d8bcbf02e6
Mario Sepulveda <mario.sepulveda1901@alumnos.ubiobio.cl>
2023-04-25 14:36:05 -0400

    Optimized core

--- MOHAAFixStutteringMultiCore.bat
+++ MOHAAFixStutteringMultiCore.bat
@@ -1,10 +1,24 @@
 @echo off
+
 color 06
-echo START
+::set a variable for the MOHAA game folder 
+set mohaaFolderDir= "C:\Program Files (x86)\GOG Galaxy\Games\Medal of Honor - Allied Assault War Chest"
+::END set variable
+
+::Menu
+echo ==================================
 echo MOHAA STUTTERING FIX
 echo EN: PRESS ANY KEY TO START THE GAME
+echo ==================================
 pause
-cd "C:\Program Files (x86)\GOG Galaxy\Games\Medal of Honor - Allied Assault War Chest"
+::End Menu
+
+::Exec MOHAA.exe
+cd %mohaaFolderDir%
 start /affinity 1 MOHAA.exe
+echo ..................................
 echo NOW, ENJOY THE GAME WITHOUT STUTTERING :D
+echo ..................................
+echo EN: PRESS ANY KEY TO CLOSE THE WINDOW
+::End Exec MOHAA.exe
 pause
